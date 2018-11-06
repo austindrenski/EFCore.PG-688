74a5c1767236d5ac72b29727f161b6f968281150
Austin Drenski <austin.drenski@gmail.com>
2018-11-05 22:39:54 -0500

    Reproduce issue reported by @Jefffrey (688)
    - See: Npgsql.EntityFrameworkCore.PostgreSQL/issues/688

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using NodaTime;
+using NpgsqlTypes;
+
+// ReSharper disable StringLiteralTypo
+namespace EFCore.PG_688
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SomeContext())
+            {
+                Console.WriteLine(ctx.SomeModel.Count());
+            }
+        }
+    }
+
+    public class SomeModel
+    {
+        public NpgsqlRange<LocalTime> Period { get; set; }
+    }
+
+    public class SomeContext : DbContext
+    {
+        public DbSet<SomeModel> SomeModel { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+            => builder.UseNpgsql(
+                "Host=localhost;Port=5432",
+                x => x.UseNodaTime().MapRange<LocalTime>("timerange"));
+
+        protected override void OnModelCreating(ModelBuilder builder)
+            => builder.ForNpgsqlHasRange("timerange", "time");
+    }
+}
